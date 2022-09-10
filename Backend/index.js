2c5ae4da835a372aa91cd42779dbc3f21c519aa0
neeleshdeep <varshneyyaman766@gmail.com>
2022-09-11 04:08:35 +0530

    first

--- Backend/index.js
+++ Backend/index.js
@@ -21,9 +21,9 @@ app.use('/api/notes',require('./routes/note.js'))
 __dirname = path.resolve();
 if(process.env.NODE_ENV === "production")
 {
-    app.use(express.static(path.join(__dirname,"/build")))
+    app.use(express.static(path.join(__dirname,"/Frontend/build")))
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"build","index.html"))
+        res.sendFile(path.resolve(__dirname,"Frontend","build","index.html"))
     })
 }
 else{
